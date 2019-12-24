445df4aa2863f7c2d9bc91b2ff9ac3c1d437d5f3
Kunal Dey <kunaldeybapi@gmail.com>
2019-12-25 02:57:39 +0530

    Creating Unit test cases

--- src/app/task/add-task.component.spec.ts
+++ src/app/task/add-task.component.spec.ts
@@ -1,23 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router, ActivatedRoute } from '@angular/router';
 import { AddTaskComponent } from './add-task.component';
+import { DebugElement } from '@angular/core';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('AddTaskComponent', () => {
-  let component: AddTaskComponent;
+  let comp: AddTaskComponent;
   let fixture: ComponentFixture<AddTaskComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddTaskComponent ]
+      declarations: [ AddTaskComponent ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddTaskComponent);
-    component = fixture.componentInstance;
+    comp = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  it(`form should be invalid`, async(() => {
+    comp.addForm.get['Task1'].setValue('');    
+    comp.addForm.get['Parent_ID'].setValue('');
+    comp.addForm.get['Start_Date'].setValue('');
+    comp.addForm.get['End_Date'].setValue('');
+    expect(comp.addForm.valid).toBeFalsy();
+  }));
 
   // it('should create', () => {
   //   expect(component).toBeTruthy();
